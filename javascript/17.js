2b792e76d75b1e1c883e18109c97fb3d11fc95de
Yoonhongsang <jay182@naver.com>
2023-10-12 20:55:31 +0900

    작업중

---
+++ javascript/17.js
@@ -0,0 +1,26 @@
+//for, while => ~하는동안(반복문이다)
+//핵심문법이니 중요!!!
+
+// for (초기값; 조건식; 증감식) {}
+
+//i라는 변수는 0부터시작할거야
+//i라는 변수가 10에 도달하기 전까지 계속할거야
+//i라는 변수는 한사이클이 돌고나면 1을 더할거야
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
+//Q.왜 ++인가
+
+//배열과 for문은 짝궁이다.
+const Array = ["one", "two", "three", "four", "five"];
+for (let i = 0; i < Array.length; i++) {
+  console.log(i);
+  console.log(Array[i]);
+}
+
+//0부터 10까지의 수중에 2의 배수만 출력하기
+for (let x = 1; x < 11; x++) {
+  if (x % 2 === 0) {
+    console.log(x + "i는 2의 배수");
+  }
+}
